0e521d9760137911234d4678792497559f93cacc
Tetiana Luchynska <tania.luchynska@gmail.com>
2021-11-07 15:21:50 +0200

    added blueprint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Tab, Tabs} from "@blueprintjs/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Tabs id="TabsExample" defaultSelectedTabId="rx" vertical={true}>
+        <Tab id="ng" title="Angular" panel={<>hello</>} />
+        <Tab id="mb" title="Ember" panel={<>hello1</>}  />
+        <Tab id="rx" title="React" panel={<>hello2</>} />
+      </Tabs>
   );
 }
 
